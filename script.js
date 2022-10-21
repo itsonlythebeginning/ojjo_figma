5404e339438878d32a1a5624caf133b3a2fb8811
itsonlythebeginning <107440223+itsonlythebeginning@users.noreply.github.com>
2022-10-21 13:55:52 +0300

    Update script.js
    vh script

--- script.js
+++ script.js
@@ -66,3 +66,10 @@ header_burger.addEventListener("click", function () {
 
 })
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+
