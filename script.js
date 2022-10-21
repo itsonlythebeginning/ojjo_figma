5d03299a55a1110472b58ccdbab7c3e1d8832f10
itsonlythebeginning <107440223+itsonlythebeginning@users.noreply.github.com>
2022-10-21 14:17:44 +0300

    js

--- script.js
+++ script.js
@@ -66,10 +66,7 @@ header_burger.addEventListener("click", function () {
 
 })
 
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
